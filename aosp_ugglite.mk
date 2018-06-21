141cdad893ce59690f3def8e1b12d4ac8177f91c
Mehmet Ali Dalverir <mdalverir1@gmail.com>
2018-06-21 23:03:14 +0300

    Update and rename crdroid.mk to aosp_ugglite.mk

--- aosp_ugglite.mk
+++ aosp_ugglite.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright 2016 The AOSP-OMS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -21,12 +21,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from ugglite device
 $(call inherit-product, device/xiaomi/ugglite/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/crdroid/config/common_full_phone.mk)
+# Inherit some common AOSP-OMS stuff.
+$(call inherit-product, vendor/aosp/common.mk)
 
 PRODUCT_DEVICE := ugglite
 PRODUCT_BRAND := Xiaomi
-PRODUCT_NAME := crdroid_ugglite
+PRODUCT_NAME := aosp_ugglite
 BOARD_VENDOR := Xiaomi
 PRODUCT_MODEL := Redmi Note 5A
 PRODUCT_MANUFACTURER := Xiaomi
@@ -35,9 +35,12 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 TARGET_VENDOR := Xiaomi
 
+# Use the latest approved GMS identifiers unless running a signed build
+ifneq ($(SIGN_BUILD),true)
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT=xiaomi/ugglite/ugglite:7.1.2/N2G47H/V9.1.2.0.NDFMIEI:user/release-keys \
-PRIVATE_BUILD_DESC="ugglite-user 7.1.2 N2G47H V9.1.2.0.NDFMIEI release-keys"
+    PRIVATE_BUILD_DESC="ugglite-user 7.1.2 N2G47H V9.1.2.0.NDFMIEI release-keys"
+endif
 
 # Unofficial build ID
 TARGET_UNOFFICIAL_BUILD_ID := Mbtt
